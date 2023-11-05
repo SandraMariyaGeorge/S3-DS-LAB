9d41009cb572e4a5e6c915a3b1d6a69fa5665de8
sandramg <sangeorge04@gmail.com>
2023-11-05 12:05:56 +0530

    dll

--- single_LL.c
+++ single_LL.c
@@ -1,110 +1,175 @@
-<<<<<<< HEAD
 #include<stdio.h>
 #include<stdlib.h>
-void dele_poss();
 struct node
 {
-    int data;
-    struct node *link;
+	int data;
+	struct node *link;
 };
-struct node *head,*new,*ptr,*temp,*ptr1;
-int pos,i;
+struct node *head,*ptr,*new,*temp,*ptr1;
+int item,pos;
+void insert();
+void insert_beg();
+void insert_end();
+void insert_pos();
+void delete_beg();
+void delete_end();
+void delete_pos();
+void display();
+void main()
+{
+	int choice,ch;
+	do
+	{
+		printf("Enter your choice :\n");
+		printf("1--insertion at beginning\n2--insertion at end\n3--insertion at particular position\n4--deletion at beginning\n5--deletion at end\n6--deletion at particular position\n7--Display\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:insert_beg();
+			break;
+			case 2:insert_end();
+			break;
+			case 3:insert_pos();
+			break;
+			case 4:delete_beg();
+			break;
+			case 5:delete_end();
+			break;
+			case 6:delete_pos();
+			break;
+			case 7:display();
+			break;
+			default:printf("Invalid choice\n");
+				
+		}
+		printf("Do you want to continue\n1--YES\t2--NO\n");
+		scanf("%d",&ch);
+	}while(ch==1);
+}
 void insert()
 {
-    int item;
-    new=(struct node *)malloc(sizeof(struct node));
-    printf("Enter the value to be inserted\n");
-    scanf("%d",&item);
-    new->data=item;
-    new->link=NULL;
+	new=(struct node*)malloc(sizeof(struct node));
+	printf("Enter the element to be inserted\n");
+	scanf("%d",&item);
+	new->data=item;
+	new->link=NULL;
 }
 void insert_beg()
 {
-    insert();
-    if(head==NULL)
-    {
-        head=new;
-    }
-    else
-    {
-        new->link=head;
-        head=new;
-    }
+	insert();
+	if(head==NULL)
+	{
+		head=new;
+	}
+	else
+	{
+		new->link=head;
+		head=new;
+	}
 }
 void insert_end()
 {
-    insert();
-    if(head==NULL)
-    {
-        head=new;
-    }
-    else
-    {
-        ptr=head;
-        while(ptr->link!=NULL)
-        {
-            ptr=ptr->link;
-        }
-        ptr->link=new;
-    }
+	insert();
+	if(head==NULL)
+	{
+		head=new;
+	}
+	else
+	{
+		ptr=head;
+		while(ptr->link!=NULL)
+		{
+			ptr=ptr->link;
+		}
+		ptr->link=new;
+	}
 }
 void insert_pos()
 {
-    printf("Enter the position to be inserted :\n");
-    scanf("%d",&pos);
-    if(pos==1)
-    {
-        insert_beg();
-    }
-    else
-    {
-        insert();
-        for(i=1;i<pos-1;i++)
-        {
-            ptr=ptr->link;
-        }
-        new->link=ptr->link;
-        ptr->link=new;
-        free(ptr);
-    }
+	int i;
+	printf("Enter the position to be inserted\n");
+	scanf("%d",&pos);
+	if(pos==1)
+	{
+		insert_beg();
+	}
+	else
+	{
+		insert();
+		ptr=head;
+		for(i=1;i<pos-1;i++)
+		{
+			ptr=ptr->link;
+		}
+		new->link=ptr->link;
+		ptr->link=new;
+	}
 }
-void dele_beg()
+void delete_beg()
 {
-    if (head==NULL)
-    {
-        printf("Linked List is Empty,Deletion not possible\n");
-    }
-    else//deletion possible
-    {
-        if (head->link==NULL)
-        {
-            temp=head;
-            printf("The Deleted item is %d\n",temp->data);
-            head=NULL;
-        }
-        else
-        {
-            temp=head;
-            printf("The Deleted item is %d\n",temp->data);
-            head=temp->link;
-            free(temp);//free temp
-        }
-    }
+	if(head==NULL)
+	{
+		printf("Deletion not possible\n");
+	}
+	else
+	{
+		
+		if(head->link==NULL)//single node
+		{
+			printf("The deleted item is %d \n ",head->data);
+			head=NULL;
+		}
+		else
+		{
+			temp=head;
+			printf("The deleted item is %d \n",temp->data);
+			head=temp->link;
+			free(temp);
+		}
+	}
 }
-void dele_pos()
+void delete_end()
 {
-    printf("Enter the position at which element should be deleted :\n");
+	if(head==NULL)
+	{
+		printf("Deletion not possible\n");
+	}
+	else
+	{
+		if(head->link==NULL)//single node
+		{
+			printf("The deleted item is %d \n ",head->data);
+			head=NULL;
+		}
+		else
+		{
+			ptr=head;
+			while(ptr->link!=NULL)
+			{
+				ptr1=ptr;
+				ptr=ptr->link;
+			}
+			printf("The deleted item is %d \n ",ptr->data);
+			ptr1->link=NULL;
+			free(ptr);
+		}
+	}
+}
+void delete_pos()
+{
+	 printf("Enter the position at which element should be deleted :\n");
     scanf("%d",&pos);
-    ptr = head; // Creating a temporary
+    ptr = head;
     int i;
     if (pos == 1) 
     {
         printf("\nElement deleted is : %d\n", ptr->data);
-        head = head->link; // Advancing the head pointer
+        head = head->link; 
         ptr->link = NULL;
-        free(ptr); // Node is deleted
+        free(ptr);
     }
-    else {
+    else 
+    {
         for (i = 1; i < pos - 1; i++) 
         {
             ptr = ptr->link;
@@ -113,280 +178,23 @@ void dele_pos()
         ptr->link = ptr->link->link;
         printf("\nElement deleted is : %d\n", temp->data);
         temp->link = NULL;
-        free(temp); // Node is deleted
-    }
-}
-void dele_end()
-{
-    if(head==NULL)
-    {
-        printf("Linked List is Empty,Deletion not possible\n");
-    }
-    else
-    {
-        if (head->link==NULL)
-        {
-            temp=head;
-            printf("The Deleted item is %d\n",temp->data);
-            head=NULL;
-        }
-        else
-        {
-            ptr=head;
-            while(ptr->link!=NULL)
-            {
-                ptr1=ptr;
-                ptr=ptr->link;
-            }
-            temp=ptr;
-            printf("The Deleted item is %d\n",temp->data);
-            ptr1->link=NULL;
-            free(temp);
-        }
-    }
+        free(temp);
+	}
 }
 void display()
 {
-    if(head==NULL)
-    {
-        printf("Linked List is Empty\n");
-    }
-    else
-    {
-        ptr=head;
-        while(ptr->link!=NULL)
-        {
-            printf("%d ",ptr->data);
-            ptr=ptr->link;
-        }
-        printf("%d",ptr->data);
-        printf("\n");
-    }
-}
-void main()
-{
-    int choice,op;
-    do
-    {
-        printf("Enter your choice : \n");
-        printf("1--insertion at beginning\t2--insertion at end\t3--insertion at particular position\n4--deletion from beginning\t5--deletion from end\t6--deletion from a particular position\n7--Display Current Linked List\n");
-        scanf("%d",&choice);
-        switch (choice)
-        {
-            case 1:insert_beg();
-            break;
-            case 2:insert_end();
-            break;
-            case 3:insert_pos();
-            break;
-            case 4:dele_beg();
-            break;
-            case 5:dele_end();
-            break;
-            case 6:dele_pos();
-            break;
-            case 7:display();
-            default:
-            break;
-        }
-        printf("DO YOU WANT TO CONTINUE\n");
-	    printf("1-YES\t\t2--NO\n");
-	    scanf("%d",&op);
-	}while(op==1);
-=======
-#include<stdio.h>
-#include<stdlib.h>
-void dele_poss();
-struct node
-{
-    int data;
-    struct node *link;
-};
-struct node *head,*new,*ptr,*temp,*ptr1;
-int pos,i;
-void insert()
-{
-    int item;
-    new=(struct node *)malloc(sizeof(struct node));
-    printf("Enter the value to be inserted\n");
-    scanf("%d",&item);
-    new->data=item;
-    new->link=NULL;
-}
-void insert_beg()
-{
-    insert();
-    if(head==NULL)
-    {
-        head=new;
-    }
-    else
-    {
-        new->link=head;
-        head=new;
-    }
-}
-void insert_end()
-{
-    insert();
-    if(head==NULL)
-    {
-        head=new;
-    }
-    else
-    {
-        ptr=head;
-        while(ptr->link!=NULL)
-        {
-            ptr=ptr->link;
-        }
-        ptr->link=new;
-    }
-}
-void insert_pos()
-{
-    printf("Enter the position to be inserted :\n");
-    scanf("%d",&pos);
-    if(pos==1)
-    {
-        insert_beg();
-    }
-    else
-    {
-        insert();
-        for(i=1;i<pos-1;i++)
-        {
-            ptr=ptr->link;
-        }
-        new->link=ptr->link;
-        ptr->link=new;
-        free(ptr);
-    }
-}
-void dele_beg()
-{
-    if (head==NULL)
-    {
-        printf("Linked List is Empty,Deletion not possible\n");
-    }
-    else//deletion possible
-    {
-        if (head->link==NULL)
-        {
-            temp=head;
-            printf("The Deleted item is %d\n",temp->data);
-            head=NULL;
-        }
-        else
-        {
-            temp=head;
-            printf("The Deleted item is %d\n",temp->data);
-            head=temp->link;
-            free(temp);//free temp
-        }
-    }
-}
-void dele_pos()
-{
-    printf("Enter the position at which element should be deleted :\n");
-    scanf("%d",&pos);
-    ptr = head; // Creating a temporary
-    int i;
-    if (pos == 1) 
-    {
-        printf("\nElement deleted is : %d\n", ptr->data);
-        head = head->link; // Advancing the head pointer
-        ptr->link = NULL;
-        free(ptr); // Node is deleted
-    }
-    else {
-        for (i = 1; i < pos - 1; i++) 
-        {
-            ptr = ptr->link;
-        }
-        temp= ptr->link;
-        ptr->link = ptr->link->link;
-        printf("\nElement deleted is : %d\n", temp->data);
-        temp->link = NULL;
-        free(temp); // Node is deleted
-    }
-}
-void dele_end()
-{
-    if(head==NULL)
-    {
-        printf("Linked List is Empty,Deletion not possible\n");
-    }
-    else
-    {
-        if (head->link==NULL)
-        {
-            temp=head;
-            printf("The Deleted item is %d\n",temp->data);
-            head=NULL;
-        }
-        else
-        {
-            ptr=head;
-            while(ptr->link!=NULL)
-            {
-                ptr1=ptr;
-                ptr=ptr->link;
-            }
-            temp=ptr;
-            printf("The Deleted item is %d\n",temp->data);
-            ptr1->link=NULL;
-            free(temp);
-        }
-    }
-}
-void display()
-{
-    if(head==NULL)
-    {
-        printf("Linked List is Empty\n");
-    }
-    else
-    {
-        ptr=head;
-        while(ptr->link!=NULL)
-        {
-            printf("%d ",ptr->data);
-            ptr=ptr->link;
-        }
-        printf("%d",ptr->data);
-        printf("\n");
-    }
-}
-void main()
-{
-    int choice,op;
-    do
-    {
-        printf("Enter your choice : \n");
-        printf("1--insertion at beginning\t2--insertion at end\t3--insertion at particular position\n4--deletion from beginning\t5--deletion from end\t6--deletion from a particular position\n7--Display Current Linked List\n");
-        scanf("%d",&choice);
-        switch (choice)
-        {
-            case 1:insert_beg();
-            break;
-            case 2:insert_end();
-            break;
-            case 3:insert_pos();
-            break;
-            case 4:dele_beg();
-            break;
-            case 5:dele_end();
-            break;
-            case 6:dele_pos();
-            break;
-            case 7:display();
-            default:
-            break;
-        }
-        printf("DO YOU WANT TO CONTINUE\n");
-	    printf("1-YES\t\t2--NO\n");
-	    scanf("%d",&op);
-	}while(op==1);
->>>>>>> b7ba6003ea1cce46bcb657820c6abb13b8e0a580
+	if(head==NULL)
+	{
+		printf("THE LINKED LIST IS EMPTY\n");
+	}
+	else
+	{
+			ptr=head;
+			while(ptr->link!=NULL)
+			{
+				printf("%d ",ptr->data);
+				ptr=ptr->link;
+			}
+			printf("%d \n",ptr->data);
+	}
 }
