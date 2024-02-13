aff3a6887befc8219f2403116f9c30b95f4c3895
sandramg <sangeorge04@gmail.com>
2024-02-13 22:54:26 +0530

    qs

--- binary_search.c
+++ binary_search.c
@@ -1,49 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-	int i,n,a[50],low,high,flag,mid,key,pos;
+	int i, n, a[50], low, high, flag, mid, key, pos;
 	printf("Enter the no. of elements\n");
-	scanf("%d",&n);
+	scanf("%d", &n);
 	printf("Enter the array elements(sorted)\n");
-	for(i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%d", &a[i]);
 	}
 	printf("Enter the element to be searched\n");
-	scanf("%d",&key);
-	low=0;
-	high=n-1;
-	flag=0;
-	for(i=0;i<n;i++)
+	scanf("%d", &key);
+	low = 0;
+	high = n - 1;
+	flag = 0;
+
+	mid = (low + high) / 2;
+	while (low <= high)
 	{
-		mid=(low+high)/2;
-		while(low<=high)
+		if (a[mid] == key)
 		{
-			if (a[mid]==key)
-			{
-				pos=mid;
-				flag=1;
-				break;
-			}
-			else if(a[mid]<key)
-			{
-				low=mid+1;
-			}
-			else
-			{
-				high=mid-1;
-			}
+			pos = mid;
+			flag = 1;
+			break;
+		}
+		else if (a[mid] < key)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
 		}
 	}
 	printf("The Entered Array is \n");
-	for(i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("a[%d]=%d\t",i,a[i]);
+		printf("a[%d]=%d\t", i, a[i]);
 	}
 	printf("\n");
-	if(flag==1)
+	if (flag == 1)
 	{
-		printf("Element found at %d\n",pos);
+		printf("Element found at %d\n", pos);
 	}
 	else
 	{
